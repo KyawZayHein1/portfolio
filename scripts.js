0ee04ef2d6ba774f0d010a8ca9f745c0d0885d80
KyawZayHein1 <kyawzayhein528@gmail.com>
2025-01-09 10:51:48 +0630

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openSubTab(evt, subTabName) {
+    var i, subtabcontent, subtabbuttons;
+    subtabcontent = document.getElementsByClassName("sub-tab-content");
+    for (i = 0; i < subtabcontent.length; i++) {
+        subtabcontent[i].style.display = "none";
+    }
+    subtabbuttons = document.getElementsByClassName("sub-tab-button");
+    for (i = 0; i < subtabbuttons.length; i++) {
+        subtabbuttons[i].className = subtabbuttons[i].className.replace(" active", "");
+    }
+    document.getElementById(subTabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
