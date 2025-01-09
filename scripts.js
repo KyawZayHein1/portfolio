e96af50a04adc9e226cd93f6710f9dec8e8628bf
KyawZayHein1 <kyawzayhein528@gmail.com>
2025-01-09 15:33:29 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
-function showTab(tabName) {
-    const tabs = document.querySelectorAll('.tab-content');
-    tabs.forEach(tab => {
-      tab.classList.remove('active');
-    });
-    const activeTab = document.getElementById(tabName);
-    activeTab.classList.add('active');
-  
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-      link.classList.remove('active');
-    });
-    const activeLink = document.querySelector(`nav ul li a[href='#${tabName}']`);
-    activeLink.classList.add('active');
+function openTab(evt, tabName) {
+  var i, tabcontent, tabbuttons;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
   }
-  
+  tabbuttons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabbuttons.length; i++) {
+      tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openSubTab(evt, subTabName) {
+  var i, subtabcontent, subtabbuttons;
+  subtabcontent = document.getElementsByClassName("sub-tab-content");
+  for (i = 0; i < subtabcontent.length; i++) {
+      subtabcontent[i].style.display = "none";
+  }
+  subtabbuttons = document.getElementsByClassName("sub-tab-button");
+  for (i = 0; i < subtabbuttons.length; i++) {
+      subtabbuttons[i].className = subtabbuttons[i].className.replace(" active", "");
+  }
+  document.getElementById(subTabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Set default tab to be open
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tab-button").click();
+});
