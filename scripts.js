cb603d7729c49ec78033282d02000c44a3ad1af9
KyawZayHein1 <kyawzayhein528@gmail.com>
2025-01-09 13:07:54 +0630

    Update styles.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,7 @@
 function openTab(evt, tabName) {
-    var i, tabcontent, tabbuttons;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tabbuttons = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabbuttons.length; i++) {
-        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => tab.classList.remove('active'));
 
-function openSubTab(evt, subTabName) {
-    var i, subtabcontent, subtabbuttons;
-    subtabcontent = document.getElementsByClassName("sub-tab-content");
-    for (i = 0; i < subtabcontent.length; i++) {
-        subtabcontent[i].style.display = "none";
-    }
-    subtabbuttons = document.getElementsByClassName("sub-tab-button");
-    for (i = 0; i < subtabbuttons.length; i++) {
-        subtabbuttons[i].className = subtabbuttons[i].className.replace(" active", "");
-    }
-    document.getElementById(subTabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    const clickedTab = document.getElementById(tabName);
+    clickedTab.classList.add('active');
 }
-
-// Set default tab to be open
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".tab-button").click();
-});
